74f2ea1e32e5c0c1728e3d0f89d8c60c859169a8
Ihor Hanich <igorganich123@gmail.com>
2024-11-06 20:18:21 +0200

    Halmos truster solution, no fuzzing

---
+++ test/truster/Truster_Fuzz.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+// Damn Vulnerable DeFi v4 (https://damnvulnerabledefi.xyz)
+pragma solidity =0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
+import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
+import "./Attacker.sol";
+
+contract TrusterChallenge is StdInvariant, Test {
+    address deployer = makeAddr("deployer");
+    address player = makeAddr("player");
+    address recovery = makeAddr("recovery");
+    
+    uint256 constant TOKENS_IN_POOL = 1_000_000e18;
+
+    DamnValuableToken public token;
+    TrusterLenderPool public pool;
+
+    /**
+     * SETS UP CHALLENGE - DO NOT TOUCH
+     */
+    function setUp() public {
+        startHoax(deployer);
+        // Deploy token
+        token = new DamnValuableToken();
+
+        // Deploy pool and fund it
+        pool = new TrusterLenderPool(token);
+        token.transfer(address(pool), TOKENS_IN_POOL);
+
+        vm.stopPrank();
+
+        targetContract(address(token));
+    }
+
+    /**
+     * CHECKS SUCCESS CONDITIONS - DO NOT TOUCH
+     */
+    function invariant_isSolved() public {
+        assert(token.balanceOf(address(pool)) == TOKENS_IN_POOL);
+    }
+}
